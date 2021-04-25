8c490b228daddf134c4d2607d2c798dff095c836
Hariks17 <hariksh.94@gmail.com>
2021-04-25 11:37:58 +0530

    Added Project

---
+++ src/test/java/com/hari/dbservices/HiksBedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.dbservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiksBedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
